721fd80f3b8b0d5cb9eaac0698a1116370f8003c
PortingVR <krzysztofgluchowski2001@gmail.com>
2024-05-17 19:05:43 +0200

    zad3

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -9,21 +9,22 @@ public class Main {
     public static void main(String[] args) {
         Worker worker1 = new Worker("Alice", 30000, 1, "2021-01-15", "Worker");
         Worker worker2 = new Worker("Bob", 32000, 2, "2021-02-10", "Worker");
-        Worker worker3 = new Worker("Charlie", 34000, 3, "2021-03-05", "Worker");
-        Worker worker4 = new Worker("Dave", 36000, 4, "2021-04-20", "Worker");
+        Worker worker3 = new Worker("Charlie", 34000, 1, "2021-03-05", "Worker");
         
-        Manager manager = new Manager("Eve", 50000, 5, "2020-11-25", "Manager");
-        
+        Manager manager = new Manager("Eve", 50000, 3, "2020-11-25", "Manager");
+
         List<Employee> employees = new ArrayList<>();
         employees.add(worker1);
         employees.add(worker2);
         employees.add(worker3);
-        employees.add(worker4);
         employees.add(manager);
         
         for (Employee employee : employees) {
-            employee.work();
-            System.out.println("- " + employee.getName() + " (ID: " + employee.hashCode() + ", Position: " + employee.getPosition() + ", Hire date: " + employee.getHireDate() + ", Salary: " + employee.getSalary() + ")");
+            System.out.println(employee.getName() + " has code: " + employee.hashCode());
         }
+        
+        System.out.println("worker3 equals worker1: " + worker3.equals(worker1));
+        System.out.println("worker3 equals worker2: " + worker3.equals(worker2));
+        System.out.println("worker3 equals manager: " + worker3.equals(manager));
     }
 }
